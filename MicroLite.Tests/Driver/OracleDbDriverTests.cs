34e6fde764dccf88b6fb4c87d7ce37dc52438454
Trevor Pilley <trev@trevorpilley.co.uk>
2014-09-16 22:10:40 +0100

    Made a start on implementing OracleDbDriver
    Still some work to do here, don't fully understand how the stored
    procedure stuff works with Oracle as yet

--- MicroLite.Tests/Driver/OracleDbDriverTests.cs
+++ MicroLite.Tests/Driver/OracleDbDriverTests.cs
@@ -1,6 +1,9 @@
 ﻿namespace MicroLite.Tests.Driver
 {
+    using System.Data;
+    using System.Data.Common;
     using MicroLite.Driver;
+    using Moq;
     using Xunit;
 
     /// <summary>
@@ -8,5 +11,52 @@
     /// </summary>
     public class OracleDbDriverTests : UnitTest
     {
+        [Fact]
+        public void BuildCommandForSqlQueryWithStoredProcedureWithoutParameters()
+        {
+            var sqlQuery = new SqlQuery("CALL GetTableContents()");
+
+            var mockDbProviderFactory = new Mock<DbProviderFactory>();
+            mockDbProviderFactory.Setup(x => x.CreateCommand()).Returns(new System.Data.SqlClient.SqlCommand());
+
+            var dbDriver = new OracleDbDriver();
+            dbDriver.DbProviderFactory = mockDbProviderFactory.Object;
+
+            var command = dbDriver.BuildCommand(sqlQuery);
+
+            // The command text should only contain the stored procedure name.
+            Assert.Equal("GetTableContents", command.CommandText);
+            Assert.Equal(CommandType.StoredProcedure, command.CommandType);
+            Assert.Equal(0, command.Parameters.Count);
+        }
+
+        [Fact]
+        public void BuildCommandForSqlQueryWithStoredProcedureWithParameters()
+        {
+            var sqlQuery = new SqlQuery(
+                "CALL GetTableContents(identifier, cust_name)",
+                100, "hello");
+
+            var mockDbProviderFactory = new Mock<DbProviderFactory>();
+            mockDbProviderFactory.Setup(x => x.CreateCommand()).Returns(new System.Data.SqlClient.SqlCommand());
+
+            var dbDriver = new OracleDbDriver();
+            dbDriver.DbProviderFactory = mockDbProviderFactory.Object;
+
+            var command = dbDriver.BuildCommand(sqlQuery);
+
+            // The command text should only contain the stored procedure name.
+            Assert.Equal("GetTableContents", command.CommandText);
+            Assert.Equal(CommandType.StoredProcedure, command.CommandType);
+            Assert.Equal(2, command.Parameters.Count);
+
+            var parameter1 = (IDataParameter)command.Parameters[0];
+            Assert.Equal("identifier", parameter1.ParameterName);
+            Assert.Equal(sqlQuery.Arguments[0], parameter1.Value);
+
+            var parameter2 = (IDataParameter)command.Parameters[1];
+            Assert.Equal("cust_name", parameter2.ParameterName);
+            Assert.Equal(sqlQuery.Arguments[1], parameter2.Value);
+        }
     }
 }
--- MicroLite/Driver/OracleDbDriver.cs
+++ MicroLite/Driver/OracleDbDriver.cs
@@ -12,16 +12,73 @@
 // -----------------------------------------------------------------------
 namespace MicroLite.Driver
 {
+    using System;
+    using System.Data;
+    using System.Reflection;
+
     /// <summary>
     /// The implementation of <see cref="IDbDriver"/> for Oracle.
     /// </summary>
     internal sealed class OracleDbDriver : DbDriver
     {
+        private static PropertyInfo bindByNameProperty;
+
         /// <summary>
         /// Initialises a new instance of the <see cref="OracleDbDriver" /> class.
         /// </summary>
         internal OracleDbDriver()
         {
+        }
+
+        protected override IDbCommand CreateCommand(SqlQuery sqlQuery)
+        {
+            var command = base.CreateCommand(sqlQuery);
+
+            SetBindByName(command);
+
+            return command;
+        }
+
+        protected override string GetCommandText(string commandText)
+        {
+            if (commandText.StartsWith("CALL", StringComparison.OrdinalIgnoreCase))
+            {
+                var openParenthesisPosition = commandText.IndexOf('(');
+
+                if (openParenthesisPosition > 4)
+                {
+                    return commandText.Substring(4, openParenthesisPosition - 4).Trim();
+                }
+                else
+                {
+                    return commandText.Substring(4, commandText.Length - 4).Trim();
+                }
+            }
+
+            return base.GetCommandText(commandText);
+        }
+
+        protected override CommandType GetCommandType(string commandText)
+        {
+            if (commandText.StartsWith("CALL", StringComparison.OrdinalIgnoreCase))
+            {
+                return CommandType.StoredProcedure;
+            }
+
+            return base.GetCommandType(commandText);
+        }
+
+        private static void SetBindByName(IDbCommand command)
+        {
+            if (bindByNameProperty == null)
+            {
+                bindByNameProperty = command.GetType().GetProperty("BindByName");
+            }
+
+            if (bindByNameProperty != null)
+            {
+                bindByNameProperty.SetValue(command, true, null);
+            }
         }
     }
 }
